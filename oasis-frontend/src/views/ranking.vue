da93d27d3c64d2c1eb21869508d0fe7d0bc66139
YukiKun <kingsnowmushroom@126.com>
2020-03-03 12:16:43 +0800

    add: 环图格式

--- oasis-frontend/src/views/ranking.vue
+++ oasis-frontend/src/views/ranking.vue
@@ -11,7 +11,7 @@
             <ve-line
             :data="yearData"
             :settings="yearSettings"
-            :extend="yearExtand"
+            :extend="yearExtend"
             :loading="load1"
             height="100%"></ve-line>
           </div>
@@ -39,12 +39,12 @@
           <el-col :span="8">
             <div class="name">
               <h2><em>Top 10</em><br /> authors with papers cite</h2>
-              <div class="chart">
+              <div class="chart" id="ring">
                 <ve-ring 
-                class="load-c"
                 :data="paperData"
                 :settings="paperSettings"
-                :events="paperExtand"
+                :extend="paperExtend"
+                :events="paperEvents"
                 :loading="load3"
                 :legend-visible="false"
                 height="100%">
@@ -57,12 +57,13 @@
 
           </el-col>
         </el-row>
-            <div class="chart word-cloud">
+            <div class="chart" id="word-cloud">
                <ve-wordcloud
-               class="load-c"
                :data="termData"
                :settings="termSettings"
-               :loading="load4"></ve-wordcloud>
+               :extend="termExtend"
+               :loading="load4">
+               </ve-wordcloud>
           </div>
       </el-main>
     </el-container>
@@ -88,17 +89,36 @@ export default {
             "#93b7e3",
             "#a5e7f0",
             "#cbb0e3"];
-
-    //Author
-        var emphasisStyle = {
+      var emphasisStyle = {
         itemStyle: {
             barBorderWidth: 1,
             shadowBlur: 10,
             shadowOffsetX: 0,
             shadowOffsetY: 0,
             shadowColor: 'rgba(0,0,0,0.5)'
+          }
+      };
+
+    //Year
+      this.yearSettings ={
+        area: true,
+        color:colors
+      }
+      this.yearExtend={
+        textStyle:{
+          color: 'white',
+        },
+        series: {
+          label: {
+            normal: {
+              show: true
+            }
+          }
         }
-    };
+      }
+
+
+    //Author
       this.auSettings = {
         stack: { 'Papers': ['Paper1','Paper2','Paper3','Paper4','Paper5'] },
       }
@@ -122,55 +142,59 @@ export default {
           formatter: function (obj) {
             let au=obj.dataIndex;
             let paper=obj.componentIndex;
-            return `<div style="border-bottom: 1px solid rgba(255,255,255,.3);font-size: 18px;padding-bottom: 7px;margin-bottom: 7px">${self.authors[au].papers[paper].title}</div><div class="tool-content" font-size: 14px; text-align:left;"><strong>Author: </strong>${self.authors[au].papers[paper].authors}<br /><strong>Year: </strong>${self.authors[au].papers[paper].year}<br /><strong>Citation: </strong>${self.authors[au].papers[paper].citication}</div>`;
+            return `<div style="border-bottom: 1px solid rgba(255,255,255,.3);font-size: 22px;padding-bottom: 7px;margin-bottom: 7px">${self.authors[au].papers[paper].title}</div><div class="tool-content" style="font-size: 18px; text-align:left;"><strong>Author: </strong>${self.authors[au].papers[paper].authors}<br /><strong>Year: </strong>${self.authors[au].papers[paper].year}<br /><strong>Citation: </strong>${self.authors[au].papers[paper].citication}</div>`;
             }
           }
         }
       this.auEvents = {
         click: function(e){
           self.auClick(e)
-        },
-        dblclick : function (e) {
-          self.name = e.type
-          console.log("db")
         }
       };
 
 
-      this.yearSettings ={
-        area: true,
-        color:colors
-      }
-      this.yearExtand={
-        textStyle:{
-          color: 'white',
-        },
-        series: {
-          label: {
-            normal: {
-              show: true
-            }
-          }
-        }
-      }
+
+    //Paper
       this.paperSettings ={
-        radius: ['88%', '75%'],
-        // offsetY:'-1%',
+        radius: ['75%', '90%'],
+        offsetY:'50%',
         // roseType:'area',
         label:{
-          normal: {
-            show: false,
-            position: 'center'
-          },
-          emphasis: {
-            show: true,
-            textStyle: {
-              fontSize: '30',
-              fontWeight: 'bold'
-            }
-          }
-        }
+          show:false,
+        },
       }
+      this.paperExtend={
+        tooltip:{
+          trigger:"item",
+          position: ['50%', '50%', '50%','50%'],
+          enterable:"ture",
+          confine:"ture",
+          padding: 10,
+          // backgroundColor: 'transparent',
+          backgroundColor:'#222',
+          borderColor: '#777',
+          borderWidth: 1,
+          extraCssText:'width:25em; white-space:pre-wrap',
+          formatter: function (obj) {
+            console.log("obj",obj)
+            let p=obj.dataIndex;
+            // let paper=obj.componentIndex;
+            return `<div style="border-bottom: 1px solid rgba(255,255,255,.3);font-size: 18px;padding-bottom: 7px;
+            margin-bottom: 7px">${self.papers[p].title}</div><div class="tool-content"
+             font-size: 14px; text-align:left;">
+             <strong>Author: </strong>${self.papers[p].authors}
+             <br /><strong>Year: </strong>${self.papers[p].year}
+             <br /><strong>Citation:
+              </strong>${self.papers[p].citication}</div>`;
+              }
+          }
+      }
+      this.paperEvents = {
+
+      }
+
+
+    //Terms
       this.termSettings ={
         color: [
             "#e01f54",
@@ -194,25 +218,17 @@ export default {
             "#d5b158",
             "#38b6b6"
         ],
-         shape: 'circle'
-
+         shape: 'cardioid',
       }
 
-      this.paperExtand = {
-         color: [
-            "#516b91",
-            "#59c4e6",
-            "#edafda",
-            "#93b7e3",
-            "#a5e7f0",
-            "#cbb0e3"
-        ],
-        textStyle:{
-          color: 'white',
+      this.termExtend={
+        series: {
+          // drawOutOfBound:true,
+          width: '100%',
+          height: '100%',
+          gridSize: 10,
         }
       }
-
-
 
       return {
         yearData:{
@@ -239,6 +255,8 @@ export default {
         // load2:false,
         // load3:false,
         // load4:false,
+        authors:[],
+        papers:[]
       }
   },
   mounted(){
@@ -285,8 +303,8 @@ export default {
         let res=Promise.all([p,w]);
         res.then(r=>{
           console.log(r[0].data,r[1].data);
-          // this.paperData.rows=r[0].data;
-
+          this.paperData.rows=r[0].data;
+          this.papers=r[0].data;
           this.load3=false;
           this.termData.rows=r[1].data;
           this.load4=false;
@@ -339,42 +357,14 @@ em{
 .bar-ranking .name{
   margin: 0 0 13% 0;
 }
-.word-cloud{
-  height: 1000px;
+.ve-wordcloud {
+  height: 1000px !important;
   width: 100%;
 }
 
 
-.ve-histogram>>>.tool-content strong{
-  font-size:150%;
+.chart>>>.tool-content strong{
+  font-size:130%;
   color:rgb(255, 253, 108);
 }
 </style>
-          // for (let i=0; i < 6; i++) {
-          //   let item=r[0].data[i];
-          //   this.yearData.rows.push({
-          //     'year':item.year.toString(),
-          //     'count':item.count.toString()
-          //   })
-          // }
-          // console.log("year",this.yearData)
-          // this.load1=false;
-          // this.auData.rows=r[1].data;
-
-
-
-          //         for(let i = 0; i<10; i++){
-          // this.auData.rows.push({
-          //   'Author':'SM',
-          //   'Paper1':Math.ceil(Math.random()*1000).toString(),
-          //   'Paper2':Math.ceil(Math.random()*1000).toString(),
-          //   'Paper3':Math.ceil(Math.random()*1000).toString(),
-          //   'Paper4':Math.ceil(Math.random()*1000).toString(),
-          //   'Paper5':Math.ceil(Math.random()*1000).toString(),
-            // 'Paper1':Math.ceil(Math.random()*1000),
-            // 'Paper2':Math.ceil(Math.random()*1000),
-            // 'Paper3':Math.ceil(Math.random()*1000),
-            // 'Paper4':Math.ceil(Math.random()*1000),
-            // 'Paper5':Math.ceil(Math.random()*1000),
-          })
-        }
