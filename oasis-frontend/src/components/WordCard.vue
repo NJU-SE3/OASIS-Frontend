a720a13fb6b8bc866f0baada2edee67e3c3600a0
YukiKun <kingsnowmushroom@126.com>
2020-04-05 16:42:10 +0800

    update：路由拦截取消

---
+++ oasis-frontend/src/components/WordCard.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="card">
+    <div class="title">
+      <span class="el-icon-data-analysis type-icon"></span>
+      <span>Relations</span>
+    </div>
+    <div class="graph" v-loading="loading">
+      <ve-wordcloud
+        :data="wordData"
+        :settings="wordSettings"
+        :extend="wordExtend">
+      </ve-wordcloud>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "graph-card",
+      props: {
+        data: Object,
+      },
+
+      data () {
+        return {
+          loading: false,
+          wordSettings:{
+            "color": [
+                "#6aa9ff",
+                "#59c4e6",
+                "#edafda",
+                "#93b7e3",
+                "#a5e7f0",
+                "#cbb0e3",
+                "#b884b3",
+                "#74e3ff",
+                "#ddb4ff",
+                "#ff68b5",
+                "#85e3d7",
+                "#5aaef0",
+                "#fd94d1"
+            ],
+          },
+          wordExtend:{
+            series: {
+              width: '100%',
+              height: '100%',
+              gridSize: 10,
+            }
+          },
+          wordData:{
+
+          }
+        }
+      },
+      mounted(){
+        this.getData()
+      },
+      methods:{
+        getData(){
+          console.log(this.data)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  ::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius:10px;
+    background: #b4bccc;
+  }
+
+  .card{
+    background-color: azure;
+    opacity: 0.7;
+    border-radius: 4px;
+    color: #4e4376;
+    height: 350px;
+    overflow: scroll;
+  }
+
+  .title{
+    font-size: large;
+    font-weight: bold;
+    padding-top: 5px;
+    padding-left: 20px;
+    text-align: left;
+  }
+
+  .type-icon {
+    font-size: large;
+    font-weight: bold;
+  }
+
+</style>
--- oasis-frontend/src/router/index.js
+++ oasis-frontend/src/router/index.js
@@ -96,38 +96,38 @@ const router= new Router({
   ],
 });
 
-  router.beforeEach((to, from, next) => {
-  if (to.name == 'Admin') {
-    next();
-  } else {
-    getRequest("/api/permission/paper").then((response) => {
-      if (response.data) {
-        next();
-      }
-      else {
-        console.log("error");
-        if (to.name != 'MainPage') {
-          Message({
-            type: "warning",
-            message: 'Data has not been initialized.',
-            center: true
-          })
-          next("/mainpage");
-        } else {
-          next();
-        }
-      }
-    })
-      .catch(err => {
-        console.log("err")
-        Message({
-          type: "error",
-          message: '服务正在疯狂恢复中，烦请稍候。',
-          center: true
-        })
-      })
-  }
+//   router.beforeEach((to, from, next) => {
+//   if (to.name == 'Admin') {
+//     next();
+//   } else {
+//     getRequest("/api/permission/paper").then((response) => {
+//       if (response.data) {
+//         next();
+//       }
+//       else {
+//         console.log("error");
+//         if (to.name != 'MainPage') {
+//           Message({
+//             type: "warning",
+//             message: 'Data has not been initialized.',
+//             center: true
+//           })
+//           next("/mainpage");
+//         } else {
+//           next();
+//         }
+//       }
+//     })
+//       .catch(err => {
+//         console.log("err")
+//         Message({
+//           type: "error",
+//           message: '服务正在疯狂恢复中，烦请稍候。',
+//           center: true
+//         })
+//       })
+//   }
 
-})
+// })
 
 export default router;
